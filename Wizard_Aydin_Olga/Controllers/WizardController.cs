9d98350732ae93e58f0bc0cdd1b7e0806d6dd517
oezbro <a-oezdemir98@web.de>
2022-01-22 17:24:48 +0100

    InitialCommit

---
+++ Wizard_Aydin_Olga/Controllers/WizardController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wizard_Aydin_Olga.Controllers
+{
+    public class WizardController : Controller
+    {
+        public static int kartenImDeck;
+        public static int anzahlSpieler;
+        public static int aktuelleRunde;
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult KartenVerteilen()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Wizard_Aydin_Olga/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Wizard_Aydin_Olga.MvcApplication" Language="C#" %>
