1504407463c98008cdeb6f2ca7d89cff80ca5f61
olgipolgi <olgawittich1912@gmail.com>
2022-01-23 16:36:10 +0100

    Fixes and Drag and Drop
    Fixed Templates, implemented drag and drop, style changes

---
+++ Wizard_Aydin_Olga/Scripts/dragndrop.js
@@ -0,0 +1,16 @@
+﻿function onDragStart(event) {
+    event.dataTransfer.setData('text/plain', event.target.id);
+    draggedElement = event.path[0];
+
+}
+function onDragOver(event) {
+    event.preventDefault();
+}
+
+function onDrop(event) {
+    const id = event.dataTransfer.getData('text');
+    const draggableElement = draggedElement;
+    const dropzone = document.getElementById("playingBoard");
+    dropzone.appendChild(draggableElement);
+    event.dataTransfer.clearData();
+}
