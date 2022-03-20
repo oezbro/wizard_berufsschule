c570ce77437620773075563746f60f9bd6ae8b00
olgipolgi <olgawittich1912@gmail.com>
2022-03-20 19:17:39 +0100

    CSS Anpassungen, paar Lightboxen eingebaut

--- Wizard_Aydin_Olga/Scripts/dragndrop.js
+++ Wizard_Aydin_Olga/Scripts/dragndrop.js
@@ -1,11 +1,16 @@
-﻿playground = document.querySelector(".playingGround");
+﻿//Class for StartView
+if (document.querySelector(".startText")) {
+    document.querySelector("body").classList.add("startView");
+}
 
+//Drag and drop for cards
+playground = document.querySelector(".playingGround");
 function onDragStart(event) {
     event.dataTransfer.setData('text/plain', event.target.id);
     draggedElement = event.path[0];
-    draggedElementParent = draggedElement.parentNode;
-
+    draggedElementParent = draggedElement.parentNode.parentNode;
 }
+
 function onDragOver(event) {
     event.preventDefault();
 }
@@ -18,11 +23,11 @@ function onDrop(event) {
     event.dataTransfer.clearData();
     if (draggedElementParent.classList.contains("turn")) {
         ownButton = draggedElementParent.querySelector(".turnOver");
-        draggedElementParent.querySelector(".turnOver").classList.add("active");
+        ownButton.classList.add("active");
         ownButton.addEventListener("click", function () {
             document.querySelector(".turn").classList.remove("turn");
             document.querySelector(".noTurn").classList.remove("noTurn");
-            draggedElementParent.querySelector(".turnOver").classList.remove("active");
+            ownButton.classList.remove("active");
         })
     } else {
         draggedElementParent.querySelector(".startEvaluation").classList.add("active");
@@ -38,3 +43,21 @@ $("#startEvaluation").click(function () {
     }).done(function (res) {
     });
 });
+
+//Remove trickcontainer
+if (document.querySelector(".confirmationTricks")) {
+    let button = document.querySelector(".confirmationTricks");
+    let buttParent = button.parentNode.parentNode;
+    button.onclick = function () {
+        buttParent.remove();
+        document.querySelector(".ownCardDeck").style.pointerEvents = "all";
+    }
+}
+
+//Remove evaluationcontainer after 3000ms
+if (document.querySelector(".lightboxEvaluation")) {
+    setTimeout(function () {
+        document.querySelector(".lightboxEvaluation").style.opacity = 0;
+        document.querySelector(".lightboxEvaluation").style.pointerEvents = 'none';
+    }, 3000);
+}
