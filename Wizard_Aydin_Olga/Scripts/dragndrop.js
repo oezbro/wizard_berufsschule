83d7244ec7dad900c2dcc4e22b7165f369137f3e
olgipolgi <olgawittich1912@gmail.com>
2022-03-30 00:04:47 +0200

    JavaScript Sticherfassung und noch son kack

--- Wizard_Aydin_Olga/Scripts/dragndrop.js
+++ Wizard_Aydin_Olga/Scripts/dragndrop.js
@@ -22,6 +22,7 @@ function onDrop(event) {
     dropzone.appendChild(draggableElement);
     event.dataTransfer.clearData();
     if (draggedElementParent.classList.contains("turn")) {
+        draggedElementParent.classList.add("noTurn");
         ownButton = draggedElementParent.querySelector(".turnOver");
         ownButton.classList.add("active");
         ownButton.addEventListener("click", function () {
@@ -30,11 +31,15 @@ function onDrop(event) {
             ownButton.classList.remove("active");
         })
     } else {
-        draggedElementParent.querySelector(".startEvaluation").classList.add("active");
+        if (draggedElementParent.querySelector(".opponentCardWrapper").children.length > 0) {
+            draggedElementParent.querySelector(".startEvaluation").classList.add("active");
+        } else {
+            draggedElementParent.querySelector(".startEvaluationFinal").classList.add("active");
+        }
     }
 }
 
-$("#startEvaluation").click(function () {
+$("#startEvaluationFinal").click(function () {
     $.ajax({
         type: "POST",
         data: JSON.stringify(model),
@@ -46,9 +51,10 @@ $("#startEvaluation").click(function () {
 
 //Remove trickcontainer
 if (document.querySelector(".confirmationTricks")) {
-    let button = document.querySelector(".confirmationTricks");
+    button = document.querySelector(".confirmationTricks");
     let buttParent = button.parentNode.parentNode;
     button.onclick = function () {
+    tricksAmount = document.querySelector(".trickamountContainer .trick input:checked").getAttribute("value");
         buttParent.remove();
         document.querySelector(".ownCardDeck").style.pointerEvents = "all";
     }
@@ -60,4 +66,73 @@ if (document.querySelector(".lightboxEvaluation")) {
         document.querySelector(".lightboxEvaluation").style.opacity = 0;
         document.querySelector(".lightboxEvaluation").style.pointerEvents = 'none';
     }, 3000);
+}
+
+// Evaluation helper
+roundNumber = document.querySelector(".playingGround").getAttribute("data-round");
+ownTricks = 0;
+opponentTricks = 0;
+trumpColor = document.querySelector(".trumpDisplay > div").classList[0];
+ownName = document.querySelector(".playerOneName").textContent;
+opponentName = document.querySelector(".playerTwoName").textContent;
+document.getElementById("startEvaluation").onclick = function () {
+    i = 0;
+    document.querySelectorAll(".playingBoard .card").forEach(card => {
+        if (i == 0) {
+            cardColor = card.getAttribute("data-color");
+            cardNumber = card.getAttribute("data-value");
+        } else {
+            if (card.getAttribute("data-color") == cardColor) {
+                if (card.getAttribute("data-value") > cardNumber) {
+                    opponentTricks = (opponentTricks + 1);
+                    changeText(opponentName);
+                } else {
+                    ownTricks = (ownTricks + 1);
+                    changeText(ownName);
+                }
+            } else if (card.getAttribute("data-color") != cardColor) {
+                if (cardColor.includes(trumpColor) && !card.getAttribute("data-color").includes(trumpColor) && card.getAttribute("data-color") != 15) {
+                    ownTricks = (ownTricks + 1);
+                    changeText(ownName);
+                } else if (!cardColor.includes(trumpColor) && card.getAttribute("data-color").includes(trumpColor) && cardNumber != 15) {
+                    opponentTricks = (opponentTricks + 1);
+                    changeText(opponentName);
+                } else if (cardColor.includes(trumpColor) && card.getAttribute("data-color").includes(trumpColor) && cardNumber != 15) {
+                    if (card.getAttribute("data-value") > cardNumber) {
+                        opponentTricks = (opponentTricks + 1);
+                        changeText(opponentName);
+                    } else {
+                        ownTricks = (ownTricks + 1);
+                        changeText(ownName);
+                    }
+                } else {
+                    ownTricks = (ownTricks + 1);
+                    changeText(ownName);
+                }
+            } else if ((cardNumber == 14 && card.getAttribute("data-value") != 14) || (cardNumber != 15 && card.getAttribute("data-value") == 15)) {
+                opponentTricks = (opponentTricks + 1);
+                changeText(opponentName);
+            } else if ((cardNumber != 14 && card.getAttribute("data-value") == 14) || (cardNumber == 14 && card.getAttribute("data-value") == 14) || (cardNumber == 15 && card.getAttribute("data-value") != 15) || (cardNumber == 15 && card.getAttribute("data-value") == 15)) {
+                ownTricks = (ownTricks + 1);
+                changeText(ownName);
+            }
+        }
+        i++;
+    });
+    startNewRound()
+}
+function changeText(name) {
+    document.querySelector(".lightboxEvaluation").style.opacity = 1;
+    document.querySelector(".lightboxEvaluation").innerHTML = '<p>' + name + ' hat den Stich gemacht!</p>' ;
+}
+
+function startNewRound() {
+    document.querySelector(".playingBoard").querySelectorAll(".card").forEach(card => {
+        card.remove();
+    })
+    document.querySelector(".ownCardDeck").style.pointerEvents = 'all';
+    document.querySelector(".ownCardDeck").classList.remove("noTurn");
+    document.querySelector(".ownCardDeck").classList.add("turn");
+    document.querySelector(".opponentsCardDeck").querySelector(".active").classList.remove("active");
+
 }
